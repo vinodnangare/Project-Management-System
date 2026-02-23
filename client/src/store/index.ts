d53ad0ee9201ef254de58086c50e8c786b913810
Vinod Nangare <vinodnangare@example.com>
2026-02-23 15:29:52 +0530

    chore: update store to support meetings module and RTK Query integration

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -3,15 +3,19 @@ import uiReducer from './slices/uiSlice';
 import authReducer from './slices/authSlice';
 import uiModalReducer from './slices/uiModalSlice';
 import { api } from '../services/api';
+import { meetingsApi } from '../modules/meetings/api/meetingsApi';
 
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
     auth: authReducer,
     uiModal: uiModalReducer,
-    [api.reducerPath]: api.reducer
+    [api.reducerPath]: api.reducer,
+    [meetingsApi.reducerPath]: meetingsApi.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(api.middleware)
+    .concat(meetingsApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
